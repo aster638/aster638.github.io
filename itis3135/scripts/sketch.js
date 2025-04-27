54967b5503ba2d8d4194e85c3382e4019b2e8e9e
Krish Iyer <iyerk638@gmail.com>
2025-04-26 22:20:22 -0400

    Update sketch.js

--- itis3135/scripts/sketch.js
+++ itis3135/scripts/sketch.js
@@ -4,6 +4,52 @@
 let particles = [];
 const num = 100;
 
+// Particle class
+class Particle {
+    constructor() {
+      this.pos = createVector(random(width), random(height));
+      this.vel = createVector(0, 0);
+      this.acc = createVector(0, 0);
+      this.maxSpeed = 2;
+      this.color = color(random(100, 255), random(100, 255), random(100, 255));
+    }
+    
+    update() {
+      // Get noise value based on position and time
+      let angle = noise(this.pos.x * 0.003, this.pos.y * 0.003, frameCount * 0.005) * TWO_PI * 2;
+      
+      // Create acceleration from angle
+      this.acc.x = cos(angle) * 0.1;
+      this.acc.y = sin(angle) * 0.1;
+      
+      // Apply acceleration to velocity
+      this.vel.add(this.acc);
+      this.vel.limit(this.maxSpeed);
+      
+      // Apply velocity to position
+      this.pos.add(this.vel);
+      
+      // Wrap around edges
+      if (this.pos.x < 0) this.pos.x = width;
+      if (this.pos.x > width) this.pos.x = 0;
+      if (this.pos.y < 0) this.pos.y = height;
+      if (this.pos.y > height) this.pos.y = 0;
+    }
+    
+    display() {
+      // Draw particle
+      noStroke();
+      fill(this.color);
+      
+      // Use mouse position to influence size
+      let distance = dist(mouseX, mouseY, this.pos.x, this.pos.y);
+      let size = map(distance, 0, 200, 8, 2);
+      size = constrain(size, 2, 8);
+      
+      ellipse(this.pos.x, this.pos.y, size, size);
+    }
+  }
+  
 // Setup function runs once when the program starts
 function setup() {
   // Create canvas inside the container div
@@ -31,51 +77,6 @@ function draw() {
   }
 }
 
-// Particle class
-class Particle {
-  constructor() {
-    this.pos = createVector(random(width), random(height));
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.maxSpeed = 2;
-    this.color = color(random(100, 255), random(100, 255), random(100, 255));
-  }
-  
-  update() {
-    // Get noise value based on position and time
-    let angle = noise(this.pos.x * 0.003, this.pos.y * 0.003, frameCount * 0.005) * TWO_PI * 2;
-    
-    // Create acceleration from angle
-    this.acc.x = cos(angle) * 0.1;
-    this.acc.y = sin(angle) * 0.1;
-    
-    // Apply acceleration to velocity
-    this.vel.add(this.acc);
-    this.vel.limit(this.maxSpeed);
-    
-    // Apply velocity to position
-    this.pos.add(this.vel);
-    
-    // Wrap around edges
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.y < 0) this.pos.y = height;
-    if (this.pos.y > height) this.pos.y = 0;
-  }
-  
-  display() {
-    // Draw particle
-    noStroke();
-    fill(this.color);
-    
-    // Use mouse position to influence size
-    let distance = dist(mouseX, mouseY, this.pos.x, this.pos.y);
-    let size = map(distance, 0, 200, 8, 2);
-    size = constrain(size, 2, 8);
-    
-    ellipse(this.pos.x, this.pos.y, size, size);
-  }
-}
 
 // Add mouse interaction
 function mouseMoved() {
